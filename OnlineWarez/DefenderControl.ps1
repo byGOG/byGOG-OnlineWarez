bab159a07258dc7b7563840918fce45d209a9b4f
GOG <Ond3rkor@gmail.com>
2025-01-07 00:19:28 +0300

    Update DefenderControl.ps1

--- OnlineWarez/DefenderControl.ps1
+++ OnlineWarez/DefenderControl.ps1
@@ -1,15 +1,39 @@
-$ErrorActionPreference = "Stop"
+# Script Başlangıcı
+$Name = "Defender Control"
+$Url = "https://github.com/byGOG/byGOG-OnlineWarez/raw/refs/heads/main/OnlineWarez/DefenderControl.rar"
+$ToolsPath = "$env:SYSTEMDRIVE\tools"
+$LocalRarFile = Join-Path -Path $ToolsPath -ChildPath "DefenderControl.rar"
+$UnrarPath = "$env:TEMP\unrarw64.exe"
+$UnrarExe = "$env:TEMP\UnRAR.exe"
 
-# CMD dosyasının URL'si
-$DownloadURL = "https://github.com/byGOG/byGOG-OnlineWarez/raw/refs/heads/main/OnlineWarez/DefenderControl.cmd"
+# Araçlar klasörü oluşturuluyor
+New-Item -Path $ToolsPath -ItemType Directory -Force | Out-Null
 
-# İndirme yolu (Geçici klasör)
-$FilePath = "$env:TEMP\DefenderControl.cmd"
+# Windows Defender hariç tutma listesine ekleniyor
+Start-Process powershell -ArgumentList "Add-MpPreference -ExclusionPath '$ToolsPath'" -Verb RunAs -Wait
 
-# Dosyayı indir
-Invoke-WebRequest -Uri $DownloadURL -OutFile $FilePath -UseBasicParsing
+# Defender Control indiriliyor
+Invoke-WebRequest -Uri $Url -OutFile $LocalRarFile
 
-# CMD dosyasını yönetici olarak çalıştır
-Start-Process -FilePath $FilePath -Verb RunAs -Wait
+# UnRAR indiriliyor ve kuruluyor
+Invoke-WebRequest -Uri "https://www.rarlab.com/rar/unrarw64.exe" -OutFile $UnrarPath
+Start-Process -FilePath $UnrarPath -ArgumentList "/S", "/D=$env:TEMP" -Wait
 
+# DefenderControl.rar dosyası çıkarılıyor
+& $UnrarExe e -p"psordum" -y $LocalRarFile "$ToolsPath\DefenderControl\"
 
+# Masaüstünde kısayol oluşturuluyor
+$ShortcutPath = "$env:PUBLIC\Desktop\DefenderControl.lnk"
+$TargetPath = "$ToolsPath\DefenderControl\dControl.exe"
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $TargetPath
+$Shortcut.Save()
+
+# Geçici dosyalar siliniyor
+Remove-Item -Path $LocalRarFile -Force
+Remove-Item -Path $UnrarPath -Force
+Remove-Item -Path $UnrarExe -Force
+
+# Kurulum tamamlandı mesajı
+Write-Host "Kurulum başarıyla tamamlandı! by GOG [sordum.net]"
