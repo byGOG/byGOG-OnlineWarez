6738eda501ba7c1a37ef37ed34c2d991ad2f7287
GOG <Ond3rkor@gmail.com>
2025-01-07 00:21:40 +0300

    Update DefenderControl.ps1

--- OnlineWarez/DefenderControl.ps1
+++ OnlineWarez/DefenderControl.ps1
@@ -7,22 +7,28 @@ $UnrarPath = "$env:TEMP\unrarw64.exe"
 $UnrarExe = "$env:TEMP\UnRAR.exe"
 
 # Araçlar klasörü oluşturuluyor
+Write-Host "Araçlar klasörü oluşturuluyor..." -ForegroundColor Yellow
 New-Item -Path $ToolsPath -ItemType Directory -Force | Out-Null
 
 # Windows Defender hariç tutma listesine ekleniyor
+Write-Host "Windows Defender hariç tutma listesine ekleniyor..." -ForegroundColor Yellow
 Start-Process powershell -ArgumentList "Add-MpPreference -ExclusionPath '$ToolsPath'" -Verb RunAs -Wait
 
 # Defender Control indiriliyor
+Write-Host "$Name indiriliyor..." -ForegroundColor Yellow
 Invoke-WebRequest -Uri $Url -OutFile $LocalRarFile
 
 # UnRAR indiriliyor ve kuruluyor
+Write-Host "UnRAR indiriliyor ve kuruluyor..." -ForegroundColor Yellow
 Invoke-WebRequest -Uri "https://www.rarlab.com/rar/unrarw64.exe" -OutFile $UnrarPath
 Start-Process -FilePath $UnrarPath -ArgumentList "/S", "/D=$env:TEMP" -Wait
 
 # DefenderControl.rar dosyası çıkarılıyor
+Write-Host "DefenderControl.rar dosyası çıkarılıyor..." -ForegroundColor Yellow
 & $UnrarExe e -p"psordum" -y $LocalRarFile "$ToolsPath\DefenderControl\"
 
 # Masaüstünde kısayol oluşturuluyor
+Write-Host "Masaüstünde kısayol oluşturuluyor..." -ForegroundColor Yellow
 $ShortcutPath = "$env:PUBLIC\Desktop\DefenderControl.lnk"
 $TargetPath = "$ToolsPath\DefenderControl\dControl.exe"
 $WshShell = New-Object -ComObject WScript.Shell
@@ -31,9 +37,10 @@ $Shortcut.TargetPath = $TargetPath
 $Shortcut.Save()
 
 # Geçici dosyalar siliniyor
+Write-Host "Geçici dosyalar siliniyor..." -ForegroundColor Yellow
 Remove-Item -Path $LocalRarFile -Force
 Remove-Item -Path $UnrarPath -Force
 Remove-Item -Path $UnrarExe -Force
 
 # Kurulum tamamlandı mesajı
-Write-Host "Kurulum başarıyla tamamlandı! by GOG [sordum.net]"
+Write-Host "Kurulum başarıyla tamamlandı! by GOG [sordum.net]" -ForegroundColor Green
