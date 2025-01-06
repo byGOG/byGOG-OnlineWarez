208f246855a5eacf1d121e7b5367449d967839e6
GOG <Ond3rkor@gmail.com>
2025-01-06 23:45:44 +0300

    Delete UAC.ps1

--- UAC.ps1
+++
@@ -1,6 +0,0 @@
-# UAC kapat
-# "ConsentPromptBehaviorAdmin" anahtarını 0 olarak ayarlıyoruz. (Yönetici onay istemini kapatır)
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 0 -Type DWord -Force
-
-# Kullanıcıya bilgi mesajı göster
-Write-Host "Yönetici onay istemi kapatıldı. UAC ayarları başarıyla güncellendi."
